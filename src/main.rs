ee46e427ee76e0b41b88120c36be6a5838ec00b5
Robert Durfee <robert.durfee.17@gmail.com>
2020-04-18 19:49:00 -0500

    basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,137 +1,32 @@
-#[derive(Debug)]
-struct ParseTree<'a> {
-    #[allow(dead_code)]
-    contents: &'a str,
-    #[allow(dead_code)]
-    children: Vec<ParseTree<'a>>
+mod parser;
+mod util;
+
+use parser::Parsable;
+use std::collections::HashMap;
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+enum Nonterminal {
+    Semester,
+    Season,
+    Year,
+    Number,
+    Space,
+    None,
 }
 
-trait Parsable<'a> {
-    fn parse(&self, input: &'a str) -> Result<ParseTree, &'static str>;
-}
-
-struct Literal<'a> {
-    value: &'a str,
-} 
-
-impl<'a> Literal<'a> {
-    #[allow(dead_code)]
-    fn new(value: &'a str) -> Self {
-        return Literal { value }
-    }
-}
-
-impl<'a> Parsable<'a> for Literal<'a> {
-    fn parse(&self, input: &'a str) -> Result<ParseTree, &'static str> {
-        if input.len() < self.value.len() {
-            return Err("Failed to match: input is shorter than value to match");
-        } else if &input[..self.value.len()] == self.value {
-            return Ok(ParseTree {
-                contents: &input[..self.value.len()],
-                children: vec![],
-            })
-        } else {
-            return Err("Failed to match: input doesn't match value");
-        }
-    }
-}
-
-struct Concatenation<'a, T: Parsable<'a>> {
-    parsables: &'a [&'a T],
-}
-
-impl<'a, T: Parsable<'a>> Concatenation<'a, T> {
-    #[allow(dead_code)]
-    fn new(parsables: &'a [&'a T]) -> Self {
-        return Concatenation { parsables }
-    }
-}
-
-impl<'a, T: Parsable<'a>> Parsable<'a> for Concatenation<'a, T> {
-    fn parse(&self, input: &'a str) -> Result<ParseTree, &'static str> {
-        let mut children: Vec<ParseTree<'a>> = vec![];
-        let mut offset: usize = 0;
-        for parsable in self.parsables {
-            match parsable.parse(&input[offset..]) {
-                Result::Ok(tree) => {
-                    offset += tree.contents.len();
-                    children.push(tree);
-                },
-                Result::Err(msg) => return Err(msg),
-            }
-        }
-        return Ok(ParseTree {
-            contents: &input[..offset],
-            children,
-        });
-    }
-}
-
-struct Alternation<'a, T: Parsable<'a>> {
-    parsables: &'a [&'a T],
-}
-
-impl<'a, T: Parsable<'a>> Alternation<'a, T> {
-    #[allow(dead_code)]
-    fn new(parsables: &'a [&'a T]) -> Self {
-        return Alternation { parsables }
-    }
-}
-
-impl<'a, T: Parsable<'a>> Parsable<'a> for Alternation<'a, T> {
-    fn parse(&self, input: &'a str) -> Result<ParseTree, &'static str> {
-        for parsable in self.parsables {
-            match parsable.parse(input) {
-                Result::Ok(tree) => {
-                    return Ok(ParseTree {
-                        contents: &input[..tree.contents.len()],
-                        children: vec![tree],
-                    });
-                },
-                Result::Err(_) => continue,
-            }
-        }
-        return Err("Failed to match: input doesn't match any values");
-    }
-}
-
-struct Repetition<'a, T: Parsable<'a>> {
-    parsable: &'a T,
-}
-
-impl<'a, T: Parsable<'a>> Repetition<'a, T> {
-    #[allow(dead_code)]
-    fn new(parsable: &'a T) -> Self {
-        return Repetition { parsable };
-    }
-}
-
-impl<'a, T: Parsable<'a>> Parsable<'a> for Repetition<'a, T> {
-    fn parse(&self, input: &'a str) -> Result<ParseTree, &'static str> {
-        let mut children: Vec<ParseTree<'a>> = vec![];
-        let mut offset: usize = 0;
-        loop {
-            match self.parsable.parse(&input[offset..]) {
-                Result::Ok(tree) => {
-                    offset += tree.contents.len();
-                    children.push(tree);
-                },
-                Result::Err(_) => {
-                    return Ok(ParseTree {
-                        contents: &input[..offset],
-                        children,
-                    });
-                },
-            }
-        }
-    }
+impl Default for Nonterminal {
+    fn default() -> Self { Nonterminal::None }
 }
 
 fn main() {
-    let literal = Literal::new("test");
-    let repetition = Repetition::new(&literal);
-    match repetition.parse("test") {
-        Result::Ok(tree) => println!("{:?}", tree),
-        Result::Err(msg) => println!("{}", msg),
+    let mut definitions: HashMap<Nonterminal, Box<dyn Parsable<_>>> = HashMap::new();
+    definitions.insert(Nonterminal::Semester, cat!(skp!(nt!(Nonterminal::Space)), nt!(Nonterminal::Season), skp!(nt!(Nonterminal::Space)), nt!(Nonterminal::Year), skp!(nt!(Nonterminal::Space))));
+    definitions.insert(Nonterminal::Season, alt!(cat!(skp!(nt!(Nonterminal::Space)), lit!("fall"), skp!(nt!(Nonterminal::Space))), cat!(skp!(nt!(Nonterminal::Space)), lit!("spring"), skp!(nt!(Nonterminal::Space)))));
+    definitions.insert(Nonterminal::Year, cat!(skp!(nt!(Nonterminal::Space)), nt!(Nonterminal::Number), skp!(nt!(Nonterminal::Space)), nt!(Nonterminal::Number), skp!(nt!(Nonterminal::Space))));
+    definitions.insert(Nonterminal::Number, alt!(lit!("0"), lit!("1"), lit!("2"), lit!("3"), lit!("4"), lit!("5"), lit!("6"), lit!("7"), lit!("8"), lit!("9")));
+    definitions.insert(Nonterminal::Space, lit!(" "));
+    match nt!(Nonterminal::Semester).parse("     spring    2 3", &definitions) {
+        Ok(tree) => println!("{:#?}", tree),
+        Err(msg) => println!("Error: {}", msg),
     }
 }
---
+++ src/parser.rs
@@ -0,0 +1,251 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::fmt::Debug;
+
+#[derive(Clone, Debug)]
+pub struct ParseTree<T: Clone + Debug + Default + Hash + PartialEq + Eq> {
+    name: T,
+    contents: String,
+    all_contents: String,
+    children: Vec<ParseTree<T>>,
+    is_nonterminal: bool,
+}
+
+impl<T: Clone + Debug + Default + Hash + PartialEq + Eq> ParseTree<T> {
+    pub fn new(name: &T, contents: &str, all_contents: &str, children: Vec<ParseTree<T>>, is_nonterminal: bool) -> Self {
+        return ParseTree {
+            name: name.clone(),
+            contents: String::from(contents),
+            all_contents: String::from(all_contents),
+            children,
+            is_nonterminal,
+        };
+    }
+}
+
+pub trait Parsable<T: Clone + Debug + Default + Hash + PartialEq + Eq> {
+    fn parse(&self, input: &str, definitions: &HashMap<T, Box<dyn Parsable<T>>>) -> Result<ParseTree<T>, String>;
+}
+
+pub struct Alternation<T> {
+    parsables: Vec<Box<dyn Parsable<T>>>,
+}
+
+impl<T> Alternation<T> {
+    pub fn new(parsables: Vec<Box<dyn Parsable<T>>>) -> Self {
+        return Alternation { parsables };
+    }
+}
+
+impl<T: Clone + Debug + Default + Hash + PartialEq + Eq> Parsable<T> for Alternation<T> {
+    fn parse(&self, input: &str, definitions: &HashMap<T, Box<dyn Parsable<T>>>) -> Result<ParseTree<T>, String> {
+        for parsable in &self.parsables {
+            match parsable.parse(input, definitions) {
+                Ok(tree) => {
+                    if tree.is_nonterminal {
+                        return Ok(ParseTree::new(&Default::default(), &tree.contents, &tree.all_contents, vec![tree.clone()], false));
+                    } else {
+                        return Ok(ParseTree::new(&Default::default(), &tree.contents, &tree.all_contents, tree.children, false));
+                    }
+                },
+                Err(_) => continue,
+            }
+        }
+        return Err(format!("Input '{}' does not match any alternation", input));
+    }
+}
+
+pub struct Concatenation<T> {
+    parsables: Vec<Box<dyn Parsable<T>>>,
+}
+
+impl<T> Concatenation<T> {
+    pub fn new(parsables: Vec<Box<dyn Parsable<T>>>) -> Self {
+        return Concatenation { parsables };
+    }
+}
+
+impl<T: Clone + Debug + Default + Hash + PartialEq + Eq> Parsable<T> for Concatenation<T> {
+    fn parse(&self, input: &str, definitions: &HashMap<T, Box<dyn Parsable<T>>>) -> Result<ParseTree<T>, String> {
+        let mut offset = 0;
+        let mut contents = String::new();
+        let mut children = Vec::new();
+        for parsable in &self.parsables {
+            match parsable.parse(&input[offset..], definitions) {
+                Ok(tree) => {
+                    offset += tree.all_contents.len();
+                    contents.push_str(&tree.contents);
+                    if tree.is_nonterminal {
+                        children.push(tree);
+                    } else {
+                        children.extend(tree.children);
+                    }
+                },
+                Err(msg) => return Err(msg),
+            }
+        }
+        return Ok(ParseTree::new(&Default::default(), &contents, &input[..offset], children, false));
+    }
+}
+
+pub struct Literal {
+    value: String,
+} 
+
+impl Literal {
+    pub fn new(value: &str) -> Self {
+        return Literal { value: String::from(value) };
+    }
+}
+
+impl<T: Clone + Debug + Default + Hash + PartialEq + Eq> Parsable<T> for Literal {
+    fn parse(&self, input: &str, _definitions: &HashMap<T, Box<dyn Parsable<T>>>) -> Result<ParseTree<T>, String> {
+        if input.len() < self.value.len() {
+            return Err(format!("Input '{}' is shorter than value '{}' to match", input, self.value));
+        } else if &input[..self.value.len()] == self.value {
+            return Ok(ParseTree::new(&Default::default(), &self.value, &self.value, Vec::new(), false));
+        } else {
+            return Err(format!("Input '{}' does not match value '{}'", input, self.value));
+        }
+    }
+}
+
+pub struct Nonterminal<T> {
+    name: T,
+}
+
+impl<T> Nonterminal<T> {
+    pub fn new(name: T) -> Self {
+        return Nonterminal { name };
+    }
+}
+
+impl<T: Clone + Debug + Default + Hash + PartialEq + Eq> Parsable<T> for Nonterminal<T> {
+    fn parse(&self, input: &str, definitions: &HashMap<T, Box<dyn Parsable<T>>>) -> Result<ParseTree<T>, String> {
+        match definitions.get(&self.name) {
+            Some(parsable) => {
+                match parsable.parse(input, definitions) {
+                    Ok(tree) => {
+                        if tree.is_nonterminal {
+                            return Ok(ParseTree::new(&self.name, &tree.contents, &tree.all_contents, vec![tree.clone()], true)); 
+                        } else {
+                            return Ok(ParseTree::new(&self.name, &tree.contents, &tree.all_contents, tree.children, true));
+                        }
+                    },
+                    Err(msg) => return Err(msg),
+                }
+            },
+            None => return Err(format!("Nonterminal '{:?}' has no matching definition", self.name)),
+        }
+    }
+}
+
+pub struct Repetition<T> {
+    parsable: Box<dyn Parsable<T>>,
+}
+
+impl<T> Repetition<T> {
+    pub fn new(parsable: Box<dyn Parsable<T>>) -> Self {
+        return Repetition { parsable };
+    }
+}
+
+impl<T: Clone + Debug + Default + Hash + PartialEq + Eq> Parsable<T> for Repetition<T> {
+    fn parse(&self, input: &str, definitions: &HashMap<T, Box<dyn Parsable<T>>>) -> Result<ParseTree<T>, String> {
+        let mut offset = 0;
+        let mut contents = String::new();
+        let mut children = Vec::new();
+        loop {
+            match self.parsable.parse(&input[offset..], definitions) {
+                Ok(tree) => {
+                    offset += tree.all_contents.len();
+                    contents.push_str(&tree.contents);
+                    if tree.is_nonterminal {
+                        children.push(tree);
+                    } else {
+                        children.extend(tree.children);
+                    }
+                },
+                Err(_) => return Ok(ParseTree::new(&Default::default(), &contents, &input[..offset], children, false)),
+            }
+        }
+    }
+}
+
+pub struct Skip<T> {
+    parsable: Box<dyn Parsable<T>>,
+}
+
+impl<T> Skip<T> {
+    pub fn new(parsable: Box<dyn Parsable<T>>) -> Self {
+        return Skip { parsable };
+    }
+}
+
+impl<T: Clone + Debug + Default + Hash + PartialEq + Eq> Parsable<T> for Skip<T> {
+    fn parse(&self, input: &str, definitions: &HashMap<T, Box<dyn Parsable<T>>>) -> Result<ParseTree<T>, String> {
+        let mut offset = 0;
+        loop {
+            match self.parsable.parse(&input[offset..], definitions) {
+                Ok(tree) => offset += tree.all_contents.len(),
+                Err(_) => return Ok(ParseTree::new(&Default::default(), "", &input[..offset], Vec::new(), false)),
+            }
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! alt {
+    ($($x:expr),*) => {{
+        let mut temp_vec = Vec::new();
+        $(
+            temp_vec.push($x);
+        )*
+        let ret: Box<dyn parser::Parsable<_>> = Box::new(parser::Alternation::new(temp_vec));
+        ret
+    }}
+}
+
+#[macro_export]
+macro_rules! cat {
+    ($($x:expr),*) => {{
+        let mut temp_vec = Vec::new();
+        $(
+            temp_vec.push($x);
+        )*
+        let ret: Box<dyn parser::Parsable<_>> = Box::new(parser::Concatenation::new(temp_vec));
+        ret
+    }}
+}
+
+#[macro_export]
+macro_rules! lit {
+    ($x:expr) => {{
+        let ret: Box<dyn parser::Parsable<_>> = Box::new(parser::Literal::new($x));
+        ret
+    }}
+}
+
+#[macro_export]
+macro_rules! nt {
+    ($x:expr) => {{
+        let ret: Box<dyn parser::Parsable<_>> = Box::new(parser::Nonterminal::new($x));
+        ret
+    }}
+}
+
+#[macro_export]
+macro_rules! rep {
+    ($x:expr) => {{
+        let ret: Box<dyn parser::Parsable<_>> = Box::new(parser::Repetition::new($x));
+        ret
+    }}
+}
+
+#[macro_export]
+macro_rules! skp {
+    ($x:expr) => {{
+        let ret: Box<dyn parser::Parsable<_>> = Box::new(parser::Skip::new($x));
+        ret
+    }}
+}
